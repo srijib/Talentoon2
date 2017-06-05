57bba598908ee091301524eb8e0ed7a18d84ef1d
MinaZakaria <engineer_minazakaria@yahoo.com>
2017-06-05 12:16:06 +0200

    design of talentprofile nwe

--- talentoon/app/Http/Controllers/UserProfile.php
+++ talentoon/app/Http/Controllers/UserProfile.php
@@ -13,10 +13,9 @@ class UserProfile extends Controller
 {
 
  public function index(Request $request){
+     $user= JWTAuth::parseToken()->toUser();
 
-$user= JWTAuth::parseToken()->toUser();
-
-
+//mentor review points
 $total_mentor_reviews_points = DB::table('mentor_reviews')
      ->join('posts', 'posts.id', '=', 'mentor_reviews.post_id')
      ->join('users', 'posts.user_id', '=', 'users.id')
@@ -26,26 +25,152 @@ $total_mentor_reviews_points = DB::table('mentor_reviews')
      ->get();
 
 
-return response()->json(['status' => 1,
+
+//user points
+$points = $total_mentor_reviews_points[0]->points;
+
+
+
+
+
+//level of user
+ $level = 0;
+
+ if($points<100){
+     $level = 1;
+ }
+ if($points>=100 && $points<200)
+ {
+     $level = 2;
+ }
+ if($points>=200 && $points<300)
+ {
+     $level = 3;
+ }
+ if($points>=300 && $points<400)
+ {
+     $level = 4;
+ }
+ if($points>=400 && $points<500)
+ {
+     $level = 5;
+ }
+ if($points>=500 && $points<600)
+ {
+     $level = 6;
+ }
+ if($points>=600 && $points<700)
+ {
+     $level = 7;
+ }
+ if($points>=700 && $points<800)
+ {
+     $level = 8;
+ }
+ if($points>=800 && $points<900)
+ {
+     $level = 9;
+ }
+ if($points>=900 && $points<1000)
+ {
+     $level = 10;
+ }
+
+
+//image of the reward
+     switch ($level) {
+         case "1":
+             $rewardimage = DB::table('rewards')
+                 ->select('first')
+                 ->get();
+             $levelname = "first";
+             break;
+         case "2":
+             $rewardimage = DB::table('rewards')
+                 ->select('second')
+                 ->get();
+             $levelname = "second";
+             break;
+         case "3":
+             $rewardimage = DB::table('rewards')
+                 ->select('third')
+                 ->get();
+             $levelname = "third";
+             break;
+         case "4":
+             $rewardimage = DB::table('rewards')
+                 ->select('fourth')
+                 ->get();
+             $levelname = "fourth";
+             break;
+         case "5":
+             $rewardimage = DB::table('rewards')
+                 ->select('fifth')
+                 ->get();
+             $levelname = "fifth";
+             break;
+         case "6":
+             $rewardimage = DB::table('rewards')
+                 ->select('sixth')
+                 ->get();
+             $levelname = "sixth";
+             break;
+         case "7":
+             $rewardimage = DB::table('rewards')
+                 ->select('seventh')
+                 ->get();
+             $levelname = "seventh";
+             break;
+         case "8":
+             $rewardimage = DB::table('rewards')
+                 ->select('eighths')
+                 ->get();
+             $levelname = "eighths";
+             break;
+         case "9":
+             $rewardimage = DB::table('rewards')
+                 ->select('ninth')
+                 ->get();
+             $levelname = "ninth";
+             break;
+         case "10":
+             $rewardimage = DB::table('rewards')
+                 ->select('tenth')
+                 ->get();
+             $levelname = "tenth";
+             break;
+         default:
+             $rewardimage = DB::table('rewards')
+                 ->select('first')
+                 ->get();
+             $levelname = "first";
+             break;
+     }
+
+
+     $rewardimage = $rewardimage[0]->$levelname;
+
+     return response()->json(['status' => 1,
                     'message' => 'user data send successfully',
                   'user_id'=>$user->id,
                 'first_name'=>$user->first_name,
                 'last_name'=>$user->last_name,
                 'image'=>$user->image,
-                'points' => $total_mentor_reviews_points,
-
+//                'points' => $total_mentor_reviews_points,
+                'points_number' =>$points,
+                'reward_image'=>$rewardimage,
+                'level' => $level
               ]);
-
-
   }
 
   public function userposts(Request $request){
 
     $user= JWTAuth::parseToken()->toUser();
 
-    $post = DB::table('posts')
-    // ->join('users', 'posts.user_id', '=','users.id' )
-    ->selectRaw('posts.*,count(likeables.id) as like_count,posts.id')
+    $my_posts = DB::table('posts')
+    ->join('users', 'posts.user_id', '=','users.id' )
+    ->selectRaw('posts.*,cast(posts.created_at as date) as formatted_created_at,count(likeables.id) as like_count,posts.id,users.first_name,users.last_name,users.image')
+    // ->join('users', 'posts.user_id', '=', 'users.id')
         ->leftJoin('likeables', function($join)
               {
                   $join->on('posts.id','=','likeables.likeable_id')
@@ -54,11 +179,34 @@ return response()->json(['status' => 1,
               ->where("posts.user_id",$user->id)
               ->groupBy('posts.id')
                 ->get();
+
+    $post_ids= DB::table('shares')
+        ->select('shares.post_id')
+        ->where("shares.user_id",$user->id)
+        ->get();
+
+    $arr=[];
+    for ($i=0; $i <count($post_ids) ; $i++) {
+        array_push($arr,$post_ids[$i]->post_id);
+    }
+
+   $shares = DB::table('posts')
+        ->join('users', 'posts.user_id', '=', 'users.id')
+        ->selectRaw('posts.* , cast(posts.created_at as date) as formatted_created_at , users.first_name , users.last_name , users.image')
+        ->whereIn("posts.id", $arr)
+        ->get();
+
 //
 // select posts.id, posts.title, count(likeables.id) like_count from posts left join likeables on
 //  likeables.likeable_id = posts.id and likeables.liked = 1 group by posts.id
 
+    $allPosts = array_merge($my_posts->toArray(),$shares->toArray());
 
+
+    foreach ($allPosts as $key => $part) {
+        $sort[$key] = strtotime($part->created_at);
+    }
+    array_multisort($sort, SORT_DESC, $allPosts);
 
 
 
@@ -68,7 +216,8 @@ return response()->json(['status' => 1,
                 'first_name'=>$user->first_name,
                 'last_name'=>$user->last_name,
                 'image'=>$user->image,
-                'post'=>$post
+                // 'post'=>$my_posts,
+                'allPosts'=>$allPosts
                 // 'count'=>$countlike
 
               ]);
@@ -83,15 +232,6 @@ return response()->json(['status' => 1,
 
   public function displayShared(){
 
-    //   $categories_id=DB::table('subscribers')
-    //       ->select('subscribers.category_id')
-    //       ->where([['subscribers.subscriber_id', '=', $user->id],['subscribers.subscribed', '=',1]])
-    //       ->get();
-    //       $arr=[];
-    //       for ($i=0; $i <count($categories_id) ; $i++) {
-    //           array_push($arr,$categories_id[$i]->category_id);
-    //       }
-
       $user= JWTAuth::parseToken()->toUser();
       $post_ids= DB::table('shares')
           ->select('shares.post_id')
@@ -103,19 +243,66 @@ return response()->json(['status' => 1,
           array_push($arr,$post_ids[$i]->post_id);
       }
 
-      $shares = DB::table('posts')
+     $shares = DB::table('posts')
           ->join('users', 'posts.user_id', '=', 'users.id')
-          ->select('posts.*','users.*')
+          ->select('posts.*','users.first_name','users.last_name','users.image')
 
           ->whereIn("posts.id", $arr)
 
           ->get();
 
+  $his_posts = DB::table('posts')
+  // ->join('users', 'posts.user_id', '=','users.id' )
+  ->selectRaw('posts.*,count(likeables.id) as like_count,posts.id')
+      ->leftJoin('likeables', function($join)
+            {
+                $join->on('posts.id','=','likeables.likeable_id')
+                ->where('likeables.liked', '=', '1');
+            })
+            ->where("posts.user_id",$user->id)
+            ->groupBy('posts.id')
+              ->get();
+    $aaa = array_merge($his_posts->toArray(),$shares->toArray());
+
+
+    foreach ($aaa as $key => $part) {
+        $sort[$key] = strtotime($part->created_at);
+    }
+    array_multisort($sort, SORT_DESC, $aaa);
+
     return response()->json(['status' => 1,
                       'message' => 'posts send successfully',
-                      'shares'=>$shares
+                      'shares'=>$shares,
+                      'his'=>$his_posts,
+                      'aaa'=>$aaa
                     ]);
   }
+  // public function displayShared(){
+  //
+  //     $user= JWTAuth::parseToken()->toUser();
+  //     $post_ids= DB::table('shares')
+  //         ->select('shares.post_id')
+  //         ->where("shares.user_id",$user->id)
+  //         ->get();
+  //
+  //     $arr=[];
+  //     for ($i=0; $i <count($post_ids) ; $i++) {
+  //         array_push($arr,$post_ids[$i]->post_id);
+  //     }
+  //
+  //     $shares = DB::table('posts')
+  //         ->join('users', 'posts.user_id', '=', 'users.id')
+  //         ->select('posts.*','users.*')
+  //
+  //         ->whereIn("posts.id", $arr)
+  //
+  //         ->get();
+  //
+  //   return response()->json(['status' => 1,
+  //                     'message' => 'posts send successfully',
+  //                     'shares'=>$shares
+  //                   ]);
+  // }
 
 
 }
--- talentoon/public/app/js/controller/userprofile.js
+++ talentoon/public/app/js/controller/userprofile.js
@@ -2,44 +2,48 @@ angular.module('myApp').controller("userprofile", function ($scope, $http, user,
 
   user.userprofile().then(function(data){
       $scope.userprofile=data.data;
-      $scope.user_points_from_mentors_reviews = $scope.userprofile.points[0].points;
-      $scope.user_level = getuserlevel($scope.userprofile.points[0].points);
-      console.log("categories array",$scope.userprofile.points[0]);
-      console.log("user profile",$scope.userprofile);
+      $scope.user_points_from_mentors_reviews = $scope.userprofile.points_number;
+      $scope.reward_image = $scope.userprofile.reward_image;
+      $scope.level = $scope.userprofile.level;
+      // $scope.user_level = getuserlevel($scope.userprofile.points[0].points);
+      // console.log("user profile pointssss are ",$scope.userprofile.points[0]);
+      console.log("user profile is",$scope.userprofile);
 	} , function(err){
 		console.log(err);
 	});
 
   user.userposts().then(function(data){
      console.log(data.data);
-    $scope.userposts=data.data.post;
-    $scope.userinfo=data.data;
-        console.log("user profile posts",$scope.userposts);
-        console.log("user profile info",$scope.userinfo);
-
-  } , function(err){
-    console.log(err);
-
-  });
-  user.displayShared().then(function(data){
-     console.log("shares",data.data.shares);
-     $scope.allPosts = data.data.shares.concat($scope.userposts);
-     console.log('all postssssssssssssssssssssssssssssssss',$scope.allPosts);
-
-        $scope.allPosts.sort(function(a,b){
-            return new Date(b.created_at) - new Date(a.created_at);
-        });
-        console.log('all postssssssssssssssssssssssssssssssss after sort',$scope.allPosts);
-
-
-     $scope.usershare=data.data.shares;
+    $scope.allPosts=data.data.allPosts;
     // $scope.userinfo=data.data;
-    //     console.log("user profile posts",$scope.userposts);
-    //     console.log("user profile info",$scope.userinfo);
+        console.log("user profile posts MINAAA",data.data.allPosts);
+        // console.log("user profile info",$scope.userinfo);
+        var d = new Date(data.data.allPosts[0].created_at);
+        console.log('ddddddddddddddddddddddddddddddd',d);
 
   } , function(err){
     console.log(err);
+
   });
+  // user.displayShared().then(function(data){
+  //    console.log("shares",data.data.shares);
+  //   //  $scope.allPosts = data.data.shares.concat($scope.userposts);
+  //   //  console.log('all postssssssssssssssssssssssssssssssss',$scope.allPosts);
+  //    //
+  //   //     $scope.allPosts.sort(function(a,b){
+  //   //         return new Date(b.created_at) - new Date(a.created_at);
+  //   //     });
+  //   //     console.log('all postssssssssssssssssssssssssssssssss after sort',$scope.allPosts);
+  //    //
+  //
+  //    $scope.usershare=data.data.shares;
+  //   // $scope.userinfo=data.data;
+  //   //     console.log("user profile posts",$scope.userposts);
+  //   //     console.log("user profile info",$scope.userinfo);
+  //
+  // } , function(err){
+  //   console.log(err);
+  // });
 
 
 })
