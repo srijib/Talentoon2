cbae6671be9753c6c3f170a14ec223113ef8fe60
NadaBayoumy <nada.bayoumy@hotmail.com>
2017-06-12 01:34:27 +0200

    Initial Review and deciding talent initial level with approve as talent, some changes in WizIQ code

--- talentoon/app/Http/Controllers/InitialReviewController.php
+++ talentoon/app/Http/Controllers/InitialReviewController.php
@@ -2,8 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\CategoryTalent;
 use App\Models\InitialReview;
 use App\Models\ReviewMedia;
+use App\Models\Role;
+use App\Models\User;
 use DB;
 
 use Illuminate\Http\Request;
@@ -212,8 +215,8 @@ class InitialReviewController extends Controller
 //            ->whereIn("review_media.id", $arr)
 //            ->get();
 
-        $all_initial_posts = DB::select(DB::raw("Select DISTINCT review_media.id as review_media_id ,review_media.*, review_media.category_talent_id as review_category_talent_id ,review_media.id as review_media_id , review_media.review_media_url,category_talents.id as category_talent_id , category_talents.* from category_talents join review_media on review_media.category_talent_id = category_talents.id left join initial_reviews IR on IR.review_media_id = review_media.id where review_media.id not in (select initial_reviews.review_media_id from initial_reviews) OR IR.mentor_id not in (select IR.mentor_id from initial_reviews) or IR.mentor_id != $mentor_id "));
-        //dd($all_initial_posts);
+        $all_initial_posts = DB::select(DB::raw("Select DISTINCT users.first_name, users.image,categories.title as category_title,review_media.id as review_media_id ,review_media.*, review_media.category_talent_id as review_category_talent_id ,review_media.id as review_media_id , review_media.review_media_url,category_talents.id as category_talent_id , category_talents.* from category_talents join users on users.id = category_talents.talent_id join categories on category_talents.category_id = categories.id join review_media on review_media.category_talent_id = category_talents.id left join initial_reviews IR on IR.review_media_id = review_media.id where review_media.id not in (select initial_reviews.review_media_id from initial_reviews) OR IR.mentor_id not in (select IR.mentor_id from initial_reviews) or IR.mentor_id != $mentor_id "));
+//        dd($all_initial_posts);
 
         return response()->json(['all_initial_posts' => $all_initial_posts,'status' => '1','message' => 'data sent successfully']);
     }
@@ -233,19 +236,168 @@ class InitialReviewController extends Controller
         // where category_talents.talent_id =6
 
 
-        $talent_media_remaining = DB::select(DB::raw("Select  review_media.id as review_media_id ,review_media.*, review_media.category_talent_id as review_category_talent_id ,review_media.id as review_media_id , review_media.review_media_url,category_talents.id as category_talent_id , category_talents.* from  category_talents join review_media on review_media.category_talent_id = category_talents.id where  review_media.id not in (select initial_reviews.review_media_id from initial_reviews)"));
+//        $talent_media_remaining = DB::select(DB::raw("Select  review_media.id as review_media_id ,review_media.*, review_media.category_talent_id as review_category_talent_id ,review_media.id as review_media_id , review_media.review_media_url,category_talents.id as category_talent_id , category_talents.* from  category_talents join review_media on review_media.category_talent_id = category_talents.id where  review_media.id not in (select initial_reviews.review_media_id from initial_reviews)"));
+//
+//
+//
+//        $talent_media_remaining = DB::table('review_media')
+//            ->join('initial_reviews','review_media.id', '=', 'initial_reviews.review_media_id')
+//            ->join('category_talents','category_talents.id' ,'=' ,'initial_reviews.category_talent_id')
+//            ->select('*')
+//            ->where('category_talents.talent_id', $talent_id)
+//            ->get();
 
 
 
-        $talent_media_remaining = DB::table('review_media')
-            ->join('initial_reviews','review_media.id', '=', 'initial_reviews.review_media_id')
-            ->join('category_talents','category_talents.id' ,'=' ,'initial_reviews.category_talent_id')
-            ->select('*')
-            ->where('category_talents.talent_id', $talent_id)
-            ->get();
 
 
-//        dd($talent_media_remaining);
+
+
+        //level detection/////////////////////////////////////////////////////
+
+        $level=0;
+        $points=0;
+
+
+        $total_mentor_reviews_points = DB::select(DB::raw("select  sum(initial_reviews.level_single) as points_level , initial_reviews.category_talent_id from `initial_reviews` where initial_reviews.category_talent_id = $request->category_talent_id  GROUP by initial_reviews.category_talent_id"));
+        //user points
+        if($total_mentor_reviews_points[0]->points_level) {
+            $points = $total_mentor_reviews_points[0]->points_level;
+
+            //level of user
+            $level = 0;
+
+            if ($points < 100) {
+                $level = 1;
+            }
+            if ($points >= 100 && $points < 200) {
+                $level = 2;
+            }
+            if ($points >= 200 && $points < 300) {
+                $level = 3;
+            }
+            if ($points >= 300 && $points < 400) {
+                $level = 4;
+            }
+            if ($points >= 400 && $points < 500) {
+                $level = 5;
+            }
+            if ($points >= 500 && $points < 600) {
+                $level = 6;
+            }
+            if ($points >= 600 && $points < 700) {
+                $level = 7;
+            }
+            if ($points >= 700 && $points < 800) {
+                $level = 8;
+            }
+            if ($points >= 800 && $points < 900) {
+                $level = 9;
+            }
+            if ($points >= 900 && $points < 1000) {
+                $level = 10;
+            }
+
+
+            //image of the reward
+            switch ($level) {
+                case "1":
+                    $rewardimage = DB::table('rewards')
+                        ->select('first')
+                        ->get();
+                    $levelname = "first";
+                    break;
+                case "2":
+                    $rewardimage = DB::table('rewards')
+                        ->select('second')
+                        ->get();
+                    $levelname = "second";
+                    break;
+                case "3":
+                    $rewardimage = DB::table('rewards')
+                        ->select('third')
+                        ->get();
+                    $levelname = "third";
+                    break;
+                case "4":
+                    $rewardimage = DB::table('rewards')
+                        ->select('fourth')
+                        ->get();
+                    $levelname = "fourth";
+                    break;
+                case "5":
+                    $rewardimage = DB::table('rewards')
+                        ->select('fifth')
+                        ->get();
+                    $levelname = "fifth";
+                    break;
+                case "6":
+                    $rewardimage = DB::table('rewards')
+                        ->select('sixth')
+                        ->get();
+                    $levelname = "sixth";
+                    break;
+                case "7":
+                    $rewardimage = DB::table('rewards')
+                        ->select('seventh')
+                        ->get();
+                    $levelname = "seventh";
+                    break;
+                case "8":
+                    $rewardimage = DB::table('rewards')
+                        ->select('eighths')
+                        ->get();
+                    $levelname = "eighths";
+                    break;
+                case "9":
+                    $rewardimage = DB::table('rewards')
+                        ->select('ninth')
+                        ->get();
+                    $levelname = "ninth";
+                    break;
+                case "10":
+                    $rewardimage = DB::table('rewards')
+                        ->select('tenth')
+                        ->get();
+                    $levelname = "tenth";
+                    break;
+                default:
+                    $rewardimage = DB::table('rewards')
+                        ->select('first')
+                        ->get();
+                    $levelname = "first";
+                    break;
+            }
+
+            $rewardimage = $rewardimage[0]->$levelname;
+
+        }
+        //level detection ended///////////////////////////////////////////////////
+
+
+        $category_talent = CategoryTalent::find($request->category_talent_id);
+        $category_talent->status = '1';
+        $category_talent->level = $level;
+        $category_talent->save();
+
+
+
+
+        DB::table('category_talents')->where('id', $request->category_talent_id )->update(['status' => 1]);
+        $role = Role::where('name', '=','talent')->get()->first();
+        $userId = DB::table('category_talents')
+            ->join('users', 'users.id', '=', 'category_talents.talent_id')
+            ->where('category_talents.id',$request->category_talent_id )
+            ->select('users.id')
+            ->first();
+        $user=User::find($userId->id);
+        try{
+            $user->attachRole($role);
+        }catch (\Exception $e){
+            var_dump($e->errorInfo);
+        }
+
+
 
 
 
