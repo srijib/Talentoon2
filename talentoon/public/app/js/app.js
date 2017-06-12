dfad532964d6d04c5abdaf471ee9f422a38b6921
EsraaMahmoudMohamed <esraamahmoud378@gmail.com>
2017-06-12 10:24:22 +0200

    change in compition

--- talentoon/public/app/js/app.js
+++ talentoon/public/app/js/app.js
@@ -5,7 +5,7 @@ angular.module("myApp").run(function($rootScope,$location,$timeout){
     AOS.init();
 
     $rootScope.CONSTANSTS={
-      baseURL:"http://172.16.3.77",
+      baseURL:"http://localhost",
       port:"8000"
     }
   // $rootScope.searchPosts=  function(){
